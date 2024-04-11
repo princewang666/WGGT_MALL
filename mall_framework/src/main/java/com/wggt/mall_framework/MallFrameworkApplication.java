40495d62dd7a8896b6648eed5337a87d857cd89c
princewang666 <1213246620@qq.com>
2024-04-11 11:03:26 +0800

    创建项目

---
+++ mall_framework/src/main/java/com/wggt/mall_framework/MallFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.wggt.mall_framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallFrameworkApplication.class, args);
+	}
+
+}
---
+++ mall_framework/src/test/java/com/wggt/mall_framework/MallFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wggt.mall_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
