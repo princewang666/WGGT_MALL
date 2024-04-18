e0586a266b704862ab43a8d8b8028f0425ba1f90
princewang666 <1213246620@qq.com>
2024-04-18 20:51:34 +0800

    使用Springdoc

---
+++ mall_framework/src/main/java/com/wggt/mall_framework/controller/PmsBrandController.java
@@ -0,0 +1,10 @@
+package com.wggt.mall_framework.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/brand")
+public class PmsBrandController {
+    
+}
